325cd0e8c4aeabc2464f79b6a78fc3019f476083
AntoanStefanov <antoine.stefanov@gmail.com>
2022-01-03 11:02:37 +0200

    installing blog app, adding home template for blog

--- blog/urls.py
+++ blog/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # views.home handles this path (route) ''
+    # views.home handles this route ''
     path('', views.home, name='blog_home'),
-    # views.about handles this path (route) 'about/'
+    # views.about handles this route 'about/'
     path('about/', views.about, name='blog_about'), 
 ]
