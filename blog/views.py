1da8759dc81bb23e6b2a422b6111bad540e1d073
AntoanStefanov <antoine.stefanov@gmail.com>
2022-01-04 23:10:15 +0200

    replaced dummy data with data from DB, formatted date

--- blog/views.py
+++ blog/views.py
@@ -1,36 +1,16 @@
 from django.shortcuts import render
 
+# .models import Post
+# .(current directory) -> models -> Post
+from blog.models import Post
+
 # Create your views here.
 # view = page
 # A view always return an HTTP Response or an Exception.
 
-# dummy data
-posts = [
-    {
-        'author': 'John',
-        'title': 'Blog Post 1',
-        'content': 'This is a blog post 1',
-        'date_posted': 'August 27, 2018'
-    },
-    {
-        'author': 'Mary',
-        'title': 'Blog Post 2',
-        'content': 'This is a blog post 2',
-        'date_posted': 'August 11, 2018'
-    },
-    {
-        'author': 'Tony',
-        'title': 'Blog Post 3',
-        'content': 'This is a blog post 3',
-        'date_posted': 'August 16, 2018'
-    }
-
-]
-
-
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all(),
     }
     # django needs the path from the templates folder in current app to the specific template
     return render(request, 'blog/home.html', context)
