c2d1a1ea0b0c3cc6decc1cb2b06ab613751b8edb
AntoanStefanov <antoine.stefanov@gmail.com>
2022-01-06 20:11:56 +0200

    added settings for MEDIA_ROOT and MEDIA_URL, created template which needs a form

--- django_blog/settings.py
+++ django_blog/settings.py
@@ -10,8 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
 from pathlib import Path
+
 from config import KEY
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -120,6 +123,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+# https://docs.djangoproject.com/en/4.0/ref/settings/#media-root
+# place to hold the uploaded images
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# MEDIA_URL -> how we will acces media through the browser
+# https://docs.djangoproject.com/en/4.0/ref/settings/#media-url
+MEDIA_URL = os.path.join('/media/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
