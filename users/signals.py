dafcaf234643b3a211f5b819129c0b613720bb07
AntoanStefanov <antoine.stefanov@gmail.com>
2022-01-06 22:59:23 +0200

    typo receiver

--- users/signals.py
+++ users/signals.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 # signal
 from django.db.models.signals import post_save
 # reciever
-from django.dispatch import reciever
+from django.dispatch import receiver
 
 # profile model to create a profile in the reciever(the function)
 from .models import Profile
@@ -16,12 +16,12 @@ from .models import Profile
 
 # we want to run this function every time a user is created.
 # https://youtu.be/FdVuKt_iuSI?list=PL-osiE80TeTtoQCKZ03TU5fNfx2UY6U4p&t=1832
-@reciever(post_save, sender=User)
+@receiver(post_save, sender=User)
 def create_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
 
 
-@reciever(post_save, sender=User)
+@receiver(post_save, sender=User)
 def save_profile(sender, instance, **kwargs):
     instance.profile.save()
