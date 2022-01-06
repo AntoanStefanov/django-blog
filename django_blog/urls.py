a02c24fc54f21dcb5626554b0f687ee814d415a5
AntoanStefanov <antoine.stefanov@gmail.com>
2022-01-06 13:56:53 +0200

    added profile: template, view, path in project's urls, added link in base template

--- django_blog/urls.py
+++ django_blog/urls.py
@@ -28,6 +28,7 @@ urlpatterns = [
     # /about will hit this empty string and go to the about page of the blog, crazy stuff
     path('', include('blog.urls')),
     path('register/', user_views.register, name='register'),
+    path('profile/', user_views.profile, name='profile'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
 ]
