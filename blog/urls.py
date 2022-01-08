f449f4ad2f499d7766184a5b0356d1bfa9d5d748
AntoanStefanov <antoine.stefanov@gmail.com>
2022-01-08 13:14:49 +0200

    adding class based view - type list

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,17 @@
 from django.urls import path
+from .views import PostListView
 from . import views
 
 urlpatterns = [
     # views.home handles this route ''
-    path('', views.home, name='blog_home'),
+    # replacing views.home with PostListView
+    # convert the class into an actual view with .as_view() method, execute it
+    path('', PostListView.as_view(), name='blog_home'),
     # views.about handles this route 'about/'
     path('about/', views.about, name='blog_about'), 
 ]
+
+
+# django checks template in this pattern <app>/<model>_<viewtype>.html -> blog/post_list.html
+# we could create a template with this naming convention and it would see that temolate
+# But we could change wihch template this view to use, let's do it with the template we have for our home view.
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 
 # .models import Post
 # .(current directory) -> models -> Post
@@ -8,7 +9,12 @@ from blog.models import Post
 # view = page
 # A view always return an HTTP Response or an Exception.
 
+# Class based views, kinds of class based views ->
+# https://youtu.be/-s7e_Fy6NRU?list=PL-osiE80TeTtoQCKZ03TU5fNfx2UY6U4p&t=66
+
+
 def home(request):
+    # function based view
     context = {
         'posts': Post.objects.all(),
     }
@@ -16,5 +22,20 @@ def home(request):
     return render(request, 'blog/home.html', context)
 
 
+class PostListView(ListView):
+    # class based view
+    # model tells our ListView what model to query in order to create the list,
+    model = Post
+
+    # replacing default template naming convention -> <app>/<model>_<viewtype>.html
+    template_name = 'blog/home.html'
+
+    # by default ListView class will do object_list = Post.objects.all()
+    # instead of posts = Post.objects.all()
+    # let the class know we want to call it 'posts' instead of 'object_list'
+    # override attribute 'context_object_name'
+    context_object_name = 'posts' # same as our function view, line 19 
+
 def about(request):
+    # function based view
     return render(request, 'blog/about.html', {'title': 'About'})
